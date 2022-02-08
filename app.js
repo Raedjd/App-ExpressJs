e5cff62d1d16089f61ff195ee4b8e3298abd2026
Your Name <you@example.com>
2022-02-08 21:55:11 +0100

    add middleware for os and product!

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/os', operatingSystemRouter) //middleware for os 
-app.use('/product', productRouter)
+app.use('/product', productRouter)  //middleware for product
 
 
 // catch 404 and forward to error handler
